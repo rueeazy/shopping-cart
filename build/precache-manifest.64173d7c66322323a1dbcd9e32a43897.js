a30be23111d7d6a48b956e82d3d0b010d4c3024f
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-01 21:25:48 -0700

    remove build

---
+++ build/precache-manifest.64173d7c66322323a1dbcd9e32a43897.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78399aa7e58375cf666d82d2d22d4b4a",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "77832c0a0eb876e11d2d",
+    "url": "/shopping-cart/static/css/main.7e08426f.chunk.css"
+  },
+  {
+    "revision": "6b1deafa8e06f75d5987",
+    "url": "/shopping-cart/static/js/2.5662c744.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/shopping-cart/static/js/2.5662c744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77832c0a0eb876e11d2d",
+    "url": "/shopping-cart/static/js/main.1cd1bdbb.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "67de03e55eccb30dcb72a43dbdaad67d",
+    "url": "/shopping-cart/static/media/background.67de03e5.jpg"
+  },
+  {
+    "revision": "849df5bb7ed2617a2e1d01c62bb33487",
+    "url": "/shopping-cart/static/media/mens-shorts.849df5bb.jpg"
+  },
+  {
+    "revision": "331591236d687478cb561a20ce73c041",
+    "url": "/shopping-cart/static/media/mens-t.33159123.jpg"
+  },
+  {
+    "revision": "3b66f9a2fe4f54d609f575fc34caa447",
+    "url": "/shopping-cart/static/media/socks.3b66f9a2.jpg"
+  },
+  {
+    "revision": "f9dfe883e56f8099a9a820791bef06cc",
+    "url": "/shopping-cart/static/media/womens-leggings.f9dfe883.jpg"
+  }
+]);

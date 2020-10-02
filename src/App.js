e9f70ba1699f7dcd240401a4624fba0d2c8c27d9
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-01 21:21:57 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './components/Home';
 import Shop from './components/Shop';

3b744b9d89485ad360f2ba7604df56d0dc30454a
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-01 17:07:50 -0700

    Updates

---
+++ precache-manifest.0161ae18ab20b69d11427ba1d176c739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1bc40adcd7ba1e9e9366b0c3b00fa1",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "181472c4652848a8a7e4",
+    "url": "/shopping-cart/static/css/main.e5c5f8b8.chunk.css"
+  },
+  {
+    "revision": "91bbffa073b27c0c3f5f",
+    "url": "/shopping-cart/static/js/2.2934f2dc.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/shopping-cart/static/js/2.2934f2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181472c4652848a8a7e4",
+    "url": "/shopping-cart/static/js/main.0846999b.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
